2e16a6f54caa14601005997d888df313371b06e0
TalhaHsn98 <syedtalha.hussain98@gmail.com>
2025-11-24 11:22:03 -0500

    Initial commit

---
+++ WebApp_UnderTheHood/WebApp_UnderTheHood/DTO/WeatherForeCastDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApp_UnderTheHood.DTO
+{
+    public class WeatherForeCastDTO
+    {
+    }
+}
---
+++ WebApp_UnderTheHood/WebApp_UnderTheHood/Pages/Shared/_LoginStatusPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp_UnderTheHood.Pages.Shared
+{
+    public class LoginStatusPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
